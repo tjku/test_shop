17f339cc835f8523deb49b3e6cf3ccb11104ac4c
tjku <contact@tomaszjankuc.com>
2018-04-08 17:35:13 +0200

    Fix Rubocop offences

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-  acts_as_tree order: "name"
+  acts_as_tree order: 'name'
 
   validates :name, presence: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -16,25 +16,25 @@ RSpec.describe Category, type: :model do
 
         grandchild = create(:category)
         child.children << grandchild
-      end 
-    end 
+      end
+    end
 
     describe '.root?' do
       it { expect(subject.root?).to be_truthy }
-    end 
+    end
 
     describe 'has 3 children' do
-      it { expect(subject.children.size).to eq 3 } 
-    end 
+      it { expect(subject.children.size).to eq 3 }
+    end
 
     describe 'has 6 descendants' do
-      it { expect(subject.descendants.size).to eq 6 } 
-    end 
+      it { expect(subject.descendants.size).to eq 6 }
+    end
 
     describe 'child has 2 siblings' do
       let(:child) { subject.children.first }
 
-      it { expect(child.siblings.size).to eq 2 } 
-    end 
+      it { expect(child.siblings.size).to eq 2 }
+    end
   end
 end
